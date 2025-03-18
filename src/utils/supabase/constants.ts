6963fc06b550d031f8decfdfd8f8263acf2b583a
ryo0705s <inspire.sasaki15@gmail.com>
2025-03-19 02:36:14 +0900

    first commit

---
+++ src/utils/supabase/constants.ts
@@ -0,0 +1,3 @@
+export const SUPABASE_URL = "https://tqyorjcoyuafhzenrtjx.supabase.co";
+export const SUPABASE_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRxeW9yamNveXVhZmh6ZW5ydGp4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkyNTU3OTEsImV4cCI6MjA1NDgzMTc5MX0.hjVJT8EDkSkg_91vfqgaQxXnJA7pqXreEy_Mayj9VuI";
