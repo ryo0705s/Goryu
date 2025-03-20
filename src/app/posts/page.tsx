4a47b4af26747025673758a3bae37d3330c17271
ryo0705s <inspire.sasaki15@gmail.com>
2025-03-20 18:33:42 +0900

    feat: Supabaseを実データに繋ぎ込み、投稿一覧を表示

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,1 +1,75 @@
-export default function postsList() {}
+"use client";
+import React from "react";
+import {
+  useDataGrid,
+  EditButton,
+  ShowButton,
+  DeleteButton,
+  List,
+} from "@refinedev/mui";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+
+export default function PostsList() {
+  const { dataGridProps } = useDataGrid();
+
+  const columns = React.useMemo<GridColDef[]>(
+    () => [
+      {
+        field: "id",
+        headerName: "ID",
+        type: "number",
+        minWidth: 50,
+        display: "flex",
+        align: "left",
+        headerAlign: "left",
+      },
+      {
+        field: "title",
+        headerName: "タイトル",
+        type: "string",
+        minWidth: 50,
+        display: "flex",
+        align: "left",
+        headerAlign: "left",
+      },
+      {
+        field: "content",
+        headerName: "詳細",
+        type: "string",
+        minWidth: 50,
+        display: "flex",
+        align: "left",
+        headerAlign: "left",
+      },
+      {
+        field: "actions",
+        headerName: "編集・削除",
+        sortable: false,
+        renderCell: function render({ row }) {
+          return (
+            <>
+              <EditButton hideText recordItemId={row.id} />
+              <ShowButton hideText recordItemId={row.id} />
+              <DeleteButton hideText recordItemId={row.id} />
+            </>
+          );
+        },
+        align: "center",
+        headerAlign: "center",
+        minWidth: 80,
+      },
+    ],
+    []
+  );
+
+  return (
+    <List>
+      <DataGrid
+        {...dataGridProps}
+        getRowId={(row) => row?.id}
+        columns={columns}
+        autoHeight
+      />
+    </List>
+  );
+}
